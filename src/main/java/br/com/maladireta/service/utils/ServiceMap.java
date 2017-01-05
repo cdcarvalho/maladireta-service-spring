af468577dfd8979546eaac79264f66c171fc6112
Cristian <cristian.macedo.carvalho@gmail.com>
2017-01-05 19:31:39 -0200

    teste basico do angular consumindo o serviço

--- src/main/java/br/com/maladireta/service/utils/ServiceMap.java
+++ src/main/java/br/com/maladireta/service/utils/ServiceMap.java
@@ -1,7 +1,9 @@
 package br.com.maladireta.service.utils;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@CrossOrigin(origins = "*")
 @RequestMapping(consumes = "application/json", produces = "application/json")
 public interface ServiceMap {
 
