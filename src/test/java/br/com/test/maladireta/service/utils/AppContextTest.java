6f1148c22646120d60529994c6b2a7bd6b4fc007
Cristian <cristian.macedo.carvalho@gmail.com>
2017-04-09 11:12:07 -0300

    Update AppContextTest.java
    removendo configuração de teste

--- src/test/java/br/com/test/maladireta/service/utils/AppContextTest.java
+++ src/test/java/br/com/test/maladireta/service/utils/AppContextTest.java
@@ -6,9 +6,9 @@ import org.springframework.context.annotation.Import;
 
 import br.com.maladireta.service.utils.AppContext;
 
-@Configuration
-@Import(value = { AppContext.class })
-@ComponentScan(basePackages = { "br.com.test.maladireta.service" })
+//@Configuration
+//@Import(value = { AppContext.class })
+//@ComponentScan(basePackages = { "br.com.test.maladireta.service" })
 public abstract class AppContextTest {
 
 }
